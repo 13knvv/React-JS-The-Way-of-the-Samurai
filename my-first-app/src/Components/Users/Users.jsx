e06bee91199a9dc43d38f6de15cf1b4da7085c15
Vitalii Kniazev <13.kn.v.v@gmail.com>
2022-03-07 18:36:24 +0300

    add axios.get.then users

--- my-first-app/src/Components/Users/Users.jsx
+++ my-first-app/src/Components/Users/Users.jsx
@@ -1,44 +1,47 @@
+import * as axios from 'axios'
 import s from './Users.module.css'
+import userPhotoInitial from '../../assets/images/userPhotoInitial.png'
 
 const Users = (props) => {
-    if (props.users.length === 0) {
-       props.setUsers( [
-            { id: 1, photoUrl: 'https://www.pngkit.com/png/full/988-9886241_hotel-computer-icons-linkedin-native-advertising-chatbot-gloucester.png', 
-            followed: true, fullName: 'Mike Knyazev', status: 'lylypplylyl', location: {city: 'Los Angeles', country: 'USA'} },
-            { id: 2, photoUrl: 'https://www.pngkit.com/png/full/988-9886241_hotel-computer-icons-linkedin-native-advertising-chatbot-gloucester.png', 
-            followed: true, fullName: 'Patmir Knyazev', status: 'lylypplylyl', location: {city: 'New York', country: 'USA'} },
-            { id: 3, photoUrl: 'https://www.pngkit.com/png/full/988-9886241_hotel-computer-icons-linkedin-native-advertising-chatbot-gloucester.png', 
-            followed: true, fullName: 'Alena Knyazeva', status: 'lylypplylyl', location: {city: 'Miami', country: 'USA'} },
-            { id: 4, photoUrl: 'https://www.pngkit.com/png/full/988-9886241_hotel-computer-icons-linkedin-native-advertising-chatbot-gloucester.png', 
-            followed: false, fullName: 'Tutuska Knyazeva', status: 'lylypplylyl', location: {city: 'Austin', country: 'USA'} },
-        ]) 
+
+    let getUsers = () => {
+        if (props.users.length === 0) {
+
+            axios
+                .get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    props.setUsers(response.data.items)
+                })
+        }
     }
-    
+
+
 
     return (
         <div>
+            <button onClick={getUsers}>Get users</button>
             {
                 props.users.map((u, i) => {
                     return <div className={s.user} key={i}>
                         <div className={s.user__photo} >
                             <div>
-                                <img className={s.user__img} src={u.photoUrl}/>
+                                <img className={s.user__img} src={u.photos.small != null ? u.photos.small : userPhotoInitial} />
                             </div>
                             <div>
-                                {u.followed  
-                                ? (<button onClick={ () => {props.unfollow(u.id)}}>Unfollow</button>) 
-                                : (<button onClick={ () => {props.follow(u.id)}}>Follow</button>)}
-                                
+                                {u.followed
+                                    ? (<button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>)
+                                    : (<button onClick={() => { props.follow(u.id) }}>Follow</button>)}
+
                             </div>
                         </div>
                         <div className={s.user__info}>
                             <div>
-                                <div className={s.user__fullName} >{u.fullName}</div>
+                                <div className={s.user__fullName} >{u.name}</div>
                                 <div>{u.status}</div>
                             </div>
                             <div className={s.user__location} >
-                                <div>{u.location.country}</div>
-                                <div>{u.location.city}</div>
+                                <div>{'u.location.country'}</div>
+                                <div>{'u.location.city'}</div>
                             </div>
                         </div>
                     </div>
